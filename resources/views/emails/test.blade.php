367d98cd2ad1299ce9cb0d3ec3e357882bce3332
jason0105git <sa98yfvs@gmail.com>
2019-10-27 20:39:39 +0200

    restore email done

--- resources/views/emails/test.blade.php
+++ resources/views/emails/test.blade.php
@@ -1,8 +1,10 @@
 <html>
     <head></head>
     <body>
-        <div style="display: table; width: 100%; text-align: center; height: 40px; font-family: 'Open Sans', Arial, sans-serif; font-size: 14px; font-weight: bold;">
-            { $sender->url }
+        <div style="display: table; width: 100%; height: 40px; font-family: 'Open Sans', Arial, sans-serif; font-size: 14px;">
+        		<p>Уважажемый пользователь, Вы запустили процедуру восстановления зароля</p>
+            <p>для ввода нового пароля пройдите по ссылке {{ $restore_link }}</p>
+            <hr />            
         </div>
     </body>
 </html>
